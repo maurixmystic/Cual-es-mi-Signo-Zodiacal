b175e4401c0b35d69dcd64b2df6b2ba8e6798e3c
Mauricio Herrera <mauri_alejandro@live.com>
2024-03-26 05:08:32 -0300

    first commit

---
+++ Saber tu signo del Zodiaco.py
@@ -0,0 +1,38 @@
+# Función para saber el signo zodiacal del usuario basado en el mes y día que ingrese
+
+def signo_zodiacal(mes, dia):
+    if (mes == 3 and dia >= 21) or (mes == 4 and dia <= 19):
+        return "Aries"
+    elif (mes == 4 and dia >= 20) or (mes == 5 and dia <= 20):
+        return "Tauro"
+    elif (mes == 5 and dia >= 21) or (mes == 6 and dia <= 20):
+        return "Géminis"
+    elif (mes == 6 and dia >= 21) or (mes == 7 and dia <= 22):
+        return "Cáncer"
+    elif (mes == 7 and dia >= 23) or (mes == 8 and dia <= 22):
+        return "Leo"
+    elif (mes == 8 and dia >= 23) or (mes == 9 and dia <= 22):
+        return "Virgo"
+    elif (mes == 9 and dia >= 23) or (mes == 10 and dia <= 22):
+        return "Libra"
+    elif (mes == 10 and dia >= 23) or (mes == 11 and dia <= 21):
+        return "Escorpio"
+    elif (mes == 11 and dia >= 22) or (mes == 12 and dia <= 21):
+        return "Sagitario"
+    elif (mes == 12 and dia >= 22) or (mes == 1 and dia <= 19):
+        return "Capricornio"
+    elif (mes == 1 and dia >= 20) or (mes == 2 and dia <= 18):
+        return "Acuario"
+    elif (mes == 2 and dia >= 19) or (mes == 3 and dia <= 20):
+        return "Piscis"
+    else:
+        return "Fecha inválida"
+
+# El Usuario debe ingresar el mes y el día
+    
+mes = int(input("Ingresa el número del mes (1-12): "))
+dia = int(input("Ingresa el día (1-31): "))
+
+# Obtener el signo zodiacal
+
+print("Tu signo zodiacal es:", signo_zodiacal(mes, dia))
